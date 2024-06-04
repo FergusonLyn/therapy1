8fdb4cc81ca77e3bf8a508cb0b9ffd39493bcb17
Esi Yankey <graceyan244@gmail.com>
2024-06-04 14:56:55 +0000

    still working on login about to work on validation and authentication

--- app/gettingstarted/forgotPassword/page.tsx
+++ app/gettingstarted/forgotPassword/page.tsx
@@ -4,18 +4,26 @@ const Password = () => {
   return (
     <div className="flexBox min-h-[90vh]">
       <div className="flex  flex-col rounded-lg bg-[#ffffff] p-7 w-[470px]  ">
-        <h3 className="mb-12 text-2xl font-bold text-center">Reset Password</h3>
-        <label htmlFor="" className="">
-          Email
-        </label>
-        <input className="input" placeholder="Enter your email" />
+        <form action="">
+          <h3 className="mb-12 text-2xl font-bold text-center">
+            Reset Password
+          </h3>
+          <label htmlFor="" className="">
+            Email
+          </label>
+          <input
+            className="input"
+            placeholder="Enter your email"
+            type="email"
+          />
 
-        <button className="bg-[#e2e2e2] w-full py-3 rounded-[10px] mb-9 text-[13px]">
-          Send Password Resest Email
-        </button>
-        <Link href="./login" className="text-center text-xl font-semibold">
-          Back to login
-        </Link>
+          <button className="bg-[#e2e2e2] w-full py-3 rounded-[10px] mb-9 text-[13px]">
+            Send Password Resest Email
+          </button>
+          <Link href="./login" className="text-center text-xl font-semibold">
+            Back to login
+          </Link>
+        </form>
       </div>
     </div>
   );
